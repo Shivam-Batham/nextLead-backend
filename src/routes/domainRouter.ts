9c971adb8662a5f884884e8c6cb419c5bb2f73b9
shivam-batham <shivambatham2610@gmail.com>
2025-09-24 21:09:16 +0530

    auth middleware added

--- src/routes/domainRouter.ts
+++ src/routes/domainRouter.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { createDomain, deleteDomain, getAllDomain, getDomain, updateDomain } from '../controller/domainController.ts';
+import { authMiddle } from '../middlewares/authMiddleware.ts';
 
 const router: Router = Router();
 
 router.route('/create').post(createDomain);
-router.route('/get').get(getDomain);
-router.route('/getAll').get(getAllDomain);
-router.route('/update').put(updateDomain);
-router.route('/delete').delete(deleteDomain);
+router.route('/get').get(authMiddle,getDomain);
+router.route('/getAll').get(authMiddle,getAllDomain);
+router.route('/update').put(authMiddle,updateDomain);
+router.route('/delete').delete(authMiddle,deleteDomain);
 
 export default router;
